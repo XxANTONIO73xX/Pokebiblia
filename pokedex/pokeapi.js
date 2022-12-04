fb1caf2ea5379bb0b3fc9a87d861743160220d17
XxANTONIO73xX <60475171+XxANTONIO73xX@users.noreply.github.com>
2022-12-04 02:32:26 -0700

    Ya muestra datos de la api

--- pokedex/pokeapi.js
+++ pokedex/pokeapi.js
@@ -4,5 +4,23 @@ $.ajax({
     data: "",
     dataType: "json"
 }).done(function(data){
-    console.log(data)
+    data.results.forEach(pokemon => {
+        let url = pokemon.url;
+        $.ajax({
+            type: "GET",
+            url: url,
+            data: "",
+            dataType: "json",
+        }).done(function(pokemonData){
+            $("#pokemon-list").append(`
+            <div class="pokemon_listed">
+                <img src="${pokemonData.sprites.front_default}" width=110px" height="90px">
+                <div class="info">
+                    <p>Nombre:${pokemonData.name}</p>
+                    <p>Peso:${pokemonData.weight}</p>
+                    <p>Altura:${pokemonData.height}</p>
+                </div>
+            </div>`)
+        });
+    });
 })
