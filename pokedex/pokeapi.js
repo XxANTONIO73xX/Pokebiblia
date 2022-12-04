7f97d5f7c271de54680e17900c93bea56b1be386
XxANTONIO73xX <60475171+XxANTONIO73xX@users.noreply.github.com>
2022-12-04 03:22:20 -0700

    ya cuenta con sonido en el hover
    pd: aveces falla por temas de la restriccion del autoplay, ya se aviso previamente.

--- pokedex/pokeapi.js
+++ pokedex/pokeapi.js
@@ -13,14 +13,20 @@ $.ajax({
             dataType: "json",
         }).done(function(pokemonData){
             $("#pokemon-list").append(`
-            <div class="pokemon_listed">
+            <div class="pokemon_listed" id="listado">
                 <img src="${pokemonData.sprites.front_default}" width=110px" height="90px">
                 <div class="info">
                     <p>Nombre:${pokemonData.name}</p>
                     <p>Peso:${pokemonData.weight}</p>
                     <p>Altura:${pokemonData.height}</p>
                 </div>
+                <audio src="../audio/menu-efect.mp3" id="menu-audio-efecto"></audio>
             </div>`)
         });
     });
+})
+
+$(document).on('mouseenter', "#listado", function(){
+    audio = $(this).find("#menu-audio-efecto");
+    audio[0].play();
 })
